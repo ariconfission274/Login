9c4872f3dcb16e960d3a3a0c43bc6c268d4a460b
Ari <ariconfission274@gmail.com>
2023-10-24 19:19:30 +0700

    first commit

---
+++ THjavascript/script.js
@@ -0,0 +1,116 @@
+// let name = "Ari"
+// document.write(name)
+
+// document.write(' nam naiiii ')
+
+// let age = 16;
+// document.write(age + " tuoi ")
+// document.write("<hr>")
+// // kieu so number
+// let number1 = 16;
+// document.writeln(number1)
+// document.write("<hr>")
+// // kieu so string
+// let chuoi1 = '10';
+// document.writeln(chuoi1)
+
+// document.writeln(number1 + chuoi1)
+
+// document.writeln(number1 + number1)
+
+// document.writeln(chuoi1 + chuoi1)
+// document.write("<hr>")
+// // kieu Boolean
+// let boolean1 = true;
+// let boolean2 = false;
+// document.writeln(boolean1)
+// document.writeln(boolean2)
+// document.writeln(!boolean1)
+// document.writeln(!boolean2)
+// document.write("<hr>")
+
+
+// // kieu Object 
+// let nquynh = {
+//     name: "Ari",
+//     age: 16,
+//     address: 'Ameri'
+
+// }
+// // console.log(nquynh);
+// document.writeln(nquynh.name);
+// document.writeln(nquynh.age)
+// // Mang Array
+// let mang = new Array();
+// mang[0] = 'B'
+// mang[1] = 'Bi'
+// mang[2] = 'Bin'
+// mang[3] = 'Binn'
+// mang[4] = 'Binny'
+// mang[5] = 'Binnyy'
+// mang[6] = 'Binnyyy'
+
+// document.write(mang)
+// document.write('<hr>')
+
+// // Null va Undefined
+
+// let unde
+// document.writeln(unde)
+
+// let rong = null
+// document.writeln(rong)
+// document.write("<hr>")
+// // hang so " const"
+
+// // toan tu 
+
+// let soA = 10
+// document.writeln(soA + 2)
+// document.writeln(soA - 2)
+// document.writeln(soA * 2)
+// document.writeln(soA % 3)
+// document.writeln("<hr>")
+
+// ++soA
+
+
+// document.writeln("<hr>")
+// var a = 100
+// var b = 250
+// document.writeln(a == b)
+// document.writeln(a <= b)
+// document.writeln(a >= b)
+// document.writeln(a != b)
+// document.writeln(a !== b)
+// document.writeln(a === b)
+// document.writeln(a > b && a < b ) 
+
+// let a = 100;
+// let b = 998;
+// document.writeln("Gia tri cua bien a la: " + ++a);
+// document.write("<hr>");
+// document.writeln("Gia tri cua bien b la: " + b++);
+// document.write("<hr>");
+// document.writeln("Gia tri cua bien b la: " + b++);
+// document.write("<hr>");
+
+
+// var c = 1;
+// var d = '1';
+// document.writeln(c==d);
+// document.write("<hr>");
+// document.writeln(c===d);
+// document.write("<hr>");
+
+
+
+var result_1 = (5 > 3) && (5 > 7);
+var result_2 = (5 > 3) || (5 < 7);
+var result_3 = !(5 < 3);
+var result_4 = (5 > 3) && (5 > 7) || (7 > 5);
+
+document.writeln(result_1 + "<hr>");
+document.writeln(result_2 + "<hr>");
+document.writeln(result_3 + "<hr>");
+document.writeln(result_4 + "<hr>");
